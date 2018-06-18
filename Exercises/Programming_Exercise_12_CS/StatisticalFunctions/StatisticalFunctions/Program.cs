c04804516e18fcedc95d27845bee42ce91d1dc20
Steven Rhodes <sdrhodes@live.com>
2018-06-18 10:26:27 -0400

    6/18

--- Exercises/Programming_Exercise_12_CS/StatisticalFunctions/StatisticalFunctions/Program.cs
+++ Exercises/Programming_Exercise_12_CS/StatisticalFunctions/StatisticalFunctions/Program.cs
@@ -35,6 +35,60 @@ namespace StatisticalFunctions
                 Console.WriteLine(i);
             }
         }
+		
+		        public static int Mean(List<int> intList)
+        {
+            return (int)intList.Average();
+        }
+
+		// to modify from Nick Terry
+        // method to give the median (halfway point) of a list of sorted integers
+        public static double Median(List<int> intList)
+        {
+            intList.Sort();
+            // integer division requires the addition of one to get the midpoint
+            if (intList.Count % 2 != 0)
+                return (intList[intList.Count / 2 + 1]);
+
+            // if the size of the list is even it needs to be split in half which requires a double casting
+            else
+            {
+                double half1 = intList[intList.Count / 2];
+                double half2 = intList[intList.Count / 2 + 1];
+                return (half1 + half2) / 2;
+            }
+        }
+        // method to get the standard deviation of a list of integers
+        public static int StandardDeviation(List<int> intList)
+        {
+            List<int> numerator = new List<int>();
+            double median = Median(intList);
+            double average = Mean(intList);
+
+            // the formula is: square root of the sum of the integer value minus the mean squared 
+            // for every value, over the number of integers in the list
+            foreach (int num in intList)
+            {
+                int temp = (int)(Math.Pow(num - average, 2));
+                numerator.Add(temp);
+            }
+            int total = numerator.Sum();
+            return (int)Math.Sqrt(total / intList.Count);
+        }
+        // method get the median absolute deviation
+        public static double MedianAD(List<int> intList)
+        {
+            intList.Sort();
+            double median = Median(intList);
+            List<int> absList = new List<int>();
+
+            // formula is median(|num - median(intList)|)
+            foreach (int num in intList)
+            {
+                absList.Add((int)Math.Abs(num - median));
+            }
+            return Median(absList);
+        }
     }
 }
 
---
+++ Exercises/Programming_Exercise_14_CS/Program.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForceCrack
+{
+    class Program
+    {
+   
+	void forceCrack(char* hash, int passLength, int maxPassLength) 
+		//modified from StackExchange
+		{
+		int i, j, k, l, m, n, o, p, q, r;
+		char string1[maxPassLength + 1];
+		char alphanum[63] = 
+			"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+			"abcdefghijklmnopqrstuvwxyz"
+			"0123456789";
+
+		for (i = 0; i <= sizeof(string1); i++) {
+			string1[i] = '\0';
+		}
+		if (passLength < maxPassLength) // 1 Length 
+		for (i = 0; i < strlen(alphanum); i++) {    // For every alphanumerical value
+			string1[0] = alphanum[i];
+			checkPass(hash, string1);       // Checks the string
+		}
+		passLength++;
+		printf("Finished passwords of length %d\n", passLength);
+		if (passLength < maxPassLength) // 2 Length
+		for (i = 0; i < strlen(alphanum); i++) {
+			string1[0] = alphanum[i];
+			for (j = 0; j < strlen(alphanum); j++) {
+				string1[1] = alphanum[j];
+				checkPass(hash, string1);
+			}
+		}
+		passLength++;
+		printf("Finished passwords of length %d\n", passLength);
+		if (passLength < maxPassLength) // 3 Length
+		for (i = 0; i < strlen(alphanum); i++) {
+			string1[0] = alphanum[i];
+			for (j = 0; j < strlen(alphanum); j++) {
+				string1[1] = alphanum[j];
+				for (k = 0; k < strlen(alphanum); k++) {
+					string1[2] = alphanum[k];
+					checkPass(hash, string1);
+				}
+			}
+		}
+	}
